7386b849773e0ac7c4259c88f08d3b696895b486
aetonsi <18366087+aetonsi@users.noreply.github.com>
2022-05-05 15:28:01 +0200

    first commit

---
+++ amiadministrator/frmMain.vb
@@ -0,0 +1,14 @@
+﻿Module frmMain
+
+    Sub Main()
+        Dim identity = Security.Principal.WindowsIdentity.GetCurrent()
+        Dim principal = New Security.Principal.WindowsPrincipal(identity)
+        Dim isElevated As Boolean = principal.IsInRole(Security.Principal.WindowsBuiltInRole.Administrator)
+        If isElevated Then
+            Console.WriteLine("yes")
+        Else
+            Console.WriteLine("no")
+        End If
+    End Sub
+
+End Module
