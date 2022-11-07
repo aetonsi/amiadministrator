1c6567b49cc5a3f39e156b86c4a30817f4dc0393
aetonsi <18366087+aetonsi@users.noreply.github.com>
2022-11-07 12:02:42 +0100

    exit code 0 if admin, exit code 1 if not

--- amiadministrator/Program.vb
+++ amiadministrator/Program.vb
@@ -6,8 +6,10 @@
         Dim isElevated As Boolean = principal.IsInRole(Security.Principal.WindowsBuiltInRole.Administrator)
         If isElevated Then
             Console.WriteLine("yes")
+            Environment.Exit(0)
         Else
             Console.WriteLine("no")
+            Environment.Exit(1)
         End If
     End Sub
 
